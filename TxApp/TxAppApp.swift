a35ad46d4fef78918d73fbdd77f431292c58da5b
Alex Smith <alex.smith.93@gmail.com>
2026-03-20 16:57:12 -0400

    Initial Commit

---
+++ TxApp/TxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TxAppApp.swift
+//  TxApp
+//
+//  Created by Alex Smith on 2026-03-20.
+//
+
+import SwiftUI
+
+@main
+struct TxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TxAppTests/TxAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TxAppTests.swift
+//  TxAppTests
+//
+//  Created by Alex Smith on 2026-03-20.
+//
+
+import Testing
+@testable import TxApp
+
+struct TxAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
